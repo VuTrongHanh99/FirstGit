d8c0aa75c437027dc12984ec5b46fed81b5af731
VuTrongHanh99 <vuhanhcntt899@gmail.com>
2019-05-14 01:32:42 +0700

    cmit swn

--- SOURCE/My Test/app/src/main/java/com/hoctap/mrhanh/mytest/MainActivity.java
+++ SOURCE/My Test/app/src/main/java/com/hoctap/mrhanh/mytest/MainActivity.java
@@ -1,7 +1,11 @@
 package com.hoctap.mrhanh.mytest;
 
+import android.annotation.SuppressLint;
+import android.preference.EditTextPreference;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.login_layout );
+        // tạo đối tượng User
+        User winUser = new User();
+        winUser.setId( 1 );
+        winUser.setPhoneNumber( "0987421421" );
+        winUser.setProfileUrl( "http://" );
+        winUser.setUsername( "HanhTP" );
+        //Toast.makeText( this, winUser.username, Toast.LENGTH_LONG ).show();
+        //Toast.makeText( this, "Hello Toast", Toast.LENGTH_LONG ).show();
+        //Sử dụng đối tượng được khai báo trong xml
+        //Bước 1 găn id cho đối tượng trong xml
+        //Bước 2 tạo tên mới và găn lại cho đôi tượng đấy
+        //
+
+
     }
+
 }
